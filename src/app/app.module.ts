11adc92cfbca25610d5226fa6e08dd72b4b824fc
nhimbeobeo <nhimbeobeo@gmail.com>
2018-01-16 16:30:22 +0200

    task b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
